e25655f9defe40d091fc71baedbac7bf8e5af848
Ecem Öztürk <ecem.ozturk@guncel.bursa>
2022-10-18 10:22:56 +0300

    images eklendi

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,18 @@
-import "./App.css";
+//import "./App.css";
+import {Navbar, Footer, Loader,Services, Transactions, Welcome} from "./components";
+
 
 const App = () => {
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-      </h1>
+    <div className="min-h-screen">
+      <div className="gradient-bg-welcome">
+        <Navbar/>
+        <Welcome/>
+      </div>
+      <Services/>
+      <Transactions/>
+      <Footer/>
+      
     </div>
   );
 };
